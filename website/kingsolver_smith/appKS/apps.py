6e05da7342ebe25231a95952b4d7c901b482b2d3
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2020-09-02 11:17:59 -0500

    first commit

---
+++ website/kingsolver_smith/appKS/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppksConfig(AppConfig):
+    name = 'appKS'
---
+++ website/kingsolver_smith/kingsolver_smith/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for kingsolver_smith project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kingsolver_smith.settings')
+
+application = get_asgi_application()
